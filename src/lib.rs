41bf27abd93852504ff350fa2210ea07f6e333ab
minus3theta <yone_j_synthesis@yahoo.co.jp>
2021-08-08 17:05:01 +0900

    Add click handler

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,8 @@
-mod utils;
+pub mod utils;
 
 use wasm_bindgen::prelude::*;
-use wasm_bindgen::JsCast;
+use wasm_bindgen::{Clamped, JsCast};
+use web_sys::ImageData;
 
 // When the `wee_alloc` feature is enabled, use `wee_alloc` as the global
 // allocator.
@@ -12,6 +13,9 @@ static ALLOC: wee_alloc::WeeAlloc = wee_alloc::WeeAlloc::INIT;
 #[wasm_bindgen]
 extern "C" {
     fn alert(s: &str);
+
+    #[wasm_bindgen(js_namespace = console)]
+    fn log(s: &str);
 }
 
 #[wasm_bindgen]
@@ -28,12 +32,38 @@ fn draw_inner() {
     const CANVAS_ID: &str = "galaxy_canvas";
     let document = web_sys::window().unwrap().document().unwrap();
     let canvas = document.get_element_by_id(CANVAS_ID).unwrap();
-    let canvas = canvas.dyn_into::<web_sys::HtmlCanvasElement>().unwrap();
-    let context = canvas
+    let canvas: web_sys::HtmlCanvasElement =
+        canvas.dyn_into::<web_sys::HtmlCanvasElement>().unwrap();
+    let context: web_sys::CanvasRenderingContext2d = canvas
         .get_context("2d")
         .unwrap()
         .unwrap()
         .dyn_into::<web_sys::CanvasRenderingContext2d>()
         .unwrap();
-    context.fill_rect(1.0, 1.0, 100.0, 100.0);
+
+    let canvas_w = canvas.width() as usize;
+    let canvas_h = canvas.height() as usize;
+
+    let mut data = Vec::with_capacity(canvas_w * canvas_h);
+    for r in 0..canvas_h {
+        for c in 0..canvas_w {
+            data.push((255 * r / canvas_h) as u8);
+            data.push((255 * c / canvas_w) as u8);
+            data.push(0);
+            data.push(255);
+        }
+    }
+    let data = ImageData::new_with_u8_clamped_array_and_sh(
+        Clamped(&mut data),
+        canvas.width(),
+        canvas.height(),
+    )
+    .unwrap();
+    context.put_image_data(&data, 0.0, 0.0).ok();
+}
+
+#[wasm_bindgen]
+pub fn on_click(x: f64, y: f64) {
+    let msg = format!("Clicked {}, {}", x, y);
+    log(&msg);
 }
--- www/index.js
+++ www/index.js
@@ -1,3 +1,13 @@
 import * as galaxy from "../pkg";
 
 galaxy.draw();
+
+const canvas = document.getElementById("galaxy_canvas");
+canvas.addEventListener("click", event => {
+  const boundingRect = canvas.getBoundingClientRect();
+
+  const canvasLeft = (event.clientX - boundingRect.left) / boundingRect.width;
+  const canvasTop = (event.clientY - boundingRect.top) / boundingRect.height;
+
+  galaxy.on_click(canvasLeft, canvasTop);
+});
